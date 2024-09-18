42b8708bffe270e1444b40f2b704ddc4208f66e6
lsstanley <lsstanley@dundee.ac.uk>
2024-09-18 19:06:07 +0100

    getting similar companies with better RAG scores

--- API/server.js
+++ API/server.js
@@ -31,28 +31,42 @@ app.get("/api/companies", async (req, res) => {
   console.log();
   res.send(companies);
 });
-
-// gets the company RAG score
-app.get("/api/companies/companyScore/:company", async (req, res) => {
-  let companyName = req.params.company;
-  const company = await db.collection("Companies").findOne({"Company Name": companyName});
+function getRAGScore(company){
   let carbonEmissions = Number(company["Carbon Emissions"]);
   let wasteManagement = Number(company["Waste Management"]);
   let sustainabilityPractices = Number(company["Sustainability Practices"]);
-  let ragScore = `"ragScore": ${(carbonEmissions+wasteManagement+sustainabilityPractices)/(30)}`;
-  res.send(ragScore);
+  let ragScore = `${(carbonEmissions+wasteManagement+sustainabilityPractices)/(30)}`;
+  return parseFloat(ragScore);
+}
+
+// gets the company RAG score
+app.get("/api/companies/companyScore/:accountNumber", async (req, res) => {
+  let accountNumber = req.params.accountNumber;
+  const company = await db.collection("Companies").findOne({"Account Number": accountNumber});
+  res.send(getRAGScore(company));
 });
 
-app.get("api/companies/similarCompanies/:company", async (req, res) => {
-  let accountNumber = req.params.company;
+app.get("/api/companies/similarCompanies/:accNo", async (req, res) => {
+  let accountNumber = req.params.accNo;
   console.log(accountNumber);
   const company = await db.collection("Companies").findOne({"Account Number": accountNumber});
+  var lowestScore = getRAGScore(company); // because the companies won't get shown if they are not equal or better
   const companies = await db.collection("Companies").find({"Spending Category": company["Spending Category"]}).toArray();
   var table = [];
   for(var i=0; i < companies.length; i++){
-    console.log(companies[i]["Company Name"]);
+    if (companies[i]["Account Number"] === company["Account Number"]){
+      continue;
+    }
+    let ragScore = getRAGScore(companies[i]);
+    if (ragScore > lowestScore){
+      console.log(`CONTENDER: ${companies[i]["Company Name"]} SCORE: ${ragScore}`);
+      companies[i]["RAG"] = parseFloat(ragScore);
+      table.push(companies[i]);
+      continue;
+    }
+    console.log(`REJECTED: ${companies[i]["Company Name"]} SCORE: ${getRAGScore(companies[i])}`);
   }
-  res.send(companies);
+  res.send(table);
 });
 
 app.post("/api/user_transactions", async (req, res) => {
