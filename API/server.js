9a47f04d31d6159f0d01f715d2c709fd6ab41e59
jtraynor012 <190010800@dundee.ac.uk>
2024-09-30 16:32:07 +0100

    fixing the server.js file

--- API/server.js
+++ API/server.js
@@ -18,7 +18,6 @@ app.use(express.json());
 app.use(cors());
 
 
-
 // connecting to mongoDB database using mongoose
 mongoose
   .connect(uri)
@@ -28,42 +27,6 @@ mongoose
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error);
   });
-
-function generateRandomAccountNumber() {
-  return Math.floor(10000000000 + Math.random() * 90000000000);
-}
-
-
-function generateRandomBusinessAccountNumber(){
-  return Math.floor(100000000 + Math.random() * 900000000);
-}
-
-function getRAGScore(company){
-  let carbonEmissions = Number(company["Carbon Emissions"]);
-  let wasteManagement = Number(company["Waste Management"]);
-  let sustainabilityPractices = Number(company["Sustainability Practices"]);
-  let ragScore = `${(carbonEmissions+wasteManagement+sustainabilityPractices)/(30)}`;
-
-  return parseFloat(ragScore);
-}
-
-/*
-my own version of pythons json.dumps() as I could not find an alternative to this on json
-*/
-function dictionaryToJson(selectedKeys, updateTable){
-  let response = "{";
-  for(let i=0; i<selectedKeys.length; i++){
-    if(i!==selectedKeys.length-1){
-      let tempString = `"${selectedKeys[i]}":"${updateTable[selectedKeys[i]]}",`;
-      response = response.concat(tempString);
-    }else{
-      let tempString = `"${selectedKeys[i]}":"${updateTable[selectedKeys[i]]}"}`;
-      response = response.concat(tempString);
-    }
-  }
-  response = JSON.parse(response);
-  return response;
-}
 
 // this is a basic root path
 app.get("/", (req, res) => {
